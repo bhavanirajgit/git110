03593c51c776fb74217594f8372719906103ac42
bhavanirajgit <bhavanirajendran90@gmail.com>
2022-10-26 23:14:05 +0530

    testone

---
+++ src/org/gittest/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package org.gittest;
+
+public class EmployeeDetails {
+	private void empName() {
+		  System.out.println("sajan"); 
+		  }
+		private void empNumber() {
+			System.out.println("9876543210");
+
+		}
+
+		public static void main(String[] args) {
+		EmployeeDetails e = new EmployeeDetails();	
+		e.empName();
+		e.empNumber();
+		}
+
+
+
+
+
+}
