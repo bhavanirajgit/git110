5f425570a5c5bedfde86c1f28c66e5cc937c49c1
bhavanirajgit <bhavanirajendran90@gmail.com>
2022-10-27 19:08:44 +0530

    rajgit

--- src/org/gittest/EmployeeDetails.java
+++ src/org/gittest/EmployeeDetails.java
@@ -1,19 +1,24 @@
 package org.gittest;
 
 public class EmployeeDetails {
-	private void empName() {
-		  System.out.println("sajan"); 
+	   private void empName() {
+		    System.out.println("sajan"); 
 		  }
 		private void empNumber() {
 			System.out.println("9876543210");
 
 		}
+      private void empId() {
+	        System.out.println("Employee Id: xxx");
 
-		public static void main(String[] args) {
-		EmployeeDetails e = new EmployeeDetails();	
-		e.empName();
-		e.empNumber();
-		}
+}
+		
+    	public static void main(String[] args) {
+		     EmployeeDetails e = new EmployeeDetails();	
+		      e.empName();
+		      e.empNumber();
+	          e.empId();
+    	}
 
 
 
